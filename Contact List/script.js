bc23715738e50c47ebad4996c8958a4a36c7160a
Celian4862 <galacticmermaid5times@protonmail.com>
2024-04-29 09:30:02 +0800

    Updated checks in script.js
    Added idPattern to prevent spaces in ID; changed validation algorithm to display all errors if user presses Submit

--- Contact List/script.js
+++ Contact List/script.js
@@ -12,12 +12,12 @@ function hide() {
 }
 
 function validateId() {
-    var id = document.getElementById('identity').value;
+    var id = document.getElementById('identity').value, idPattern = /^[0-9]+$/;
     var errorSpan = document.getElementById('idError');
 
     if (id.length == 0) {
         errorSpan.textContent = "ID cannot be empty.";
-    } else if (isNaN(id)) {
+    } else if (!idPattern.test(id) || isNaN(id)) {
         errorSpan.textContent = "ID must be a number";
     } else if (id.length != 8) {
         errorSpan.textContent = "ID must be at least 8 digits long";
@@ -82,24 +82,25 @@ function validateContact() {
 }
 
 function validateForm() {
-    var id = document.getElementById('identity').value, idError = document.getElementById('idError');
+    var id = document.getElementById('identity').value, idPattern = /^[0-9]+$/, idError = document.getElementById('idError');
     var firstName = document.getElementById('firstName').value, firstNameError = document.getElementById('firstNameError');
     var lastName = document.getElementById('lastName').value, lastNameError = document.getElementById('lastNameError');
     var namePattern = /^[a-zA-Z ]+$/;
     var email = document.getElementById('email').value;
     var emailPattern = /^[a-zA-Z0-9.]+@[a-zA-Z.]+\.[a-zA-Z]{2,6}$/;
     var contact = document.getElementById('contact').value, contactError = document.getElementById('contactError');
+    var valid = true;
 
     // Validates ID
     if (id.length == 0) {
         idError.textContent = "ID cannot be empty.";
-        return false;
-    } else if (isNaN(id)) {
+        valid = false;
+    } else if (!idPattern.test(id) || isNaN(id)) {
         idError.textContent = "ID must be a number";
-        return false;
+        valid = false;
     } else if (id.length != 8) {
         idError.textContent = "ID must be at least 8 characters long";
-        return false;
+        valid = false;
     } else {
         idError.textContent = "";
     }
@@ -108,23 +109,23 @@ function validateForm() {
     if (firstName.length == 0 && lastName.length == 0) {
         firstNameError.textContent = "Both names cannot be empty at the same time.";
         lastNameError.textContent = "Both names cannot be empty at the same time.";
-        return false;
+        valid = false;
     } else {
         if (firstName.length > 0 && !namePattern.test(firstName)) {
             firstNameError.textContent = "Name must only contain letters.";
-            return false;
+            valid = false;
         } else if (firstName.length > 30) {
             firstNameError.textContent = "Name must be within 30 characters long.";
-            return false;
+            valid = false;
         } else {
             firstNameError.textContent = "";
         }
         if (lastName.length > 0 && !namePattern.test(lastName)) {
             lastNameError.textContent = "Name must only contain letters.";
-            return false;
+            valid = false;
         } else if (lastName.length > 30) {
             lastNameError.textContent = "Name must be within 30 characters long.";
-            return false;
+            valid = false;
         } else {
             lastNameError.textContent = "";
         }
@@ -133,10 +134,10 @@ function validateForm() {
     // Validates email
     if (email.length > 0 && !emailPattern.test(email)) {
         emailError.textContent = "Invalid email format.";
-        return false;
+        valid = false;
     } else if (email.length > 50) {
         emailError.textContent = "Email must be within 50 characters.";
-        return false;
+        valid = false;
     } else {
         emailError.textContent = "";
     }
@@ -144,13 +145,13 @@ function validateForm() {
     // Validates contact
     if (isNaN(contact)) {
         contactError.textContent = "Contact number must only contain numbers.";
-        return false;
+        valid = false;
     } else if (contact.length > 0 && contact.length != 11) {
         contactError.textContent = "Contact number must be 11 digits long.";
-        return false;
+        valid = false;
     } else {
         contactError.textContent = "";
     }
 
-    return true;
+    return valid;
 }
