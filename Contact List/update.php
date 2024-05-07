b3665dcc1710bed375f653ba670e5a0360f051fb
StruggleCore333 <143235845+StruggleCore333@users.noreply.github.com>
2024-05-07 13:53:59 +0800

    Update update.php

--- Contact List/update.php
+++ Contact List/update.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="style.css">
@@ -23,43 +23,37 @@
         $id = $row['id']; // Needed later for validation
     ?>
     <body>
-        <h1>Update Record</h1>
-        <div class='flex-container' style='padding: 3%;'>
-            <form class='center' action='save_update.php' method='POST' onsubmit='return validateForm()'>
-                    <input type='hidden' id='id' name='id' value='<?php echo $row['id'] ?>'>
-
+        <h1>Contactlist with Database </h1>
+        <div class="container">
+            <form class="center" action="save.php" method="POST" onsubmit="return validateForm()">
+                <input type="hidden" id="id" name="id" value="<?php echo $row['id'] ?>">
                 <div>
-                    <label for='firstName'>First Name:</label>
-                    <input type='text' id='firstName' name='firstName' value='<?php echo $row['firstname'] ?>' oninput='validateName()' required><br />
-                    <span class='error-message' id='firstNameError'></span>
-                    <br />
+                    <label for="firstName">First Name:</label>
+                    <input type="text" id="firstName" name="firstName" value="<?php echo $row['firstname'] ?>" oninput="validateName()" required><br>
+                    <span class="error-message" id="firstNameError"></span><br>
                 </div>
-
                 <div>
-                    <label for='lastName'>Last Name:</label>
-                    <input type='test' id='lastName' name='lastName' value='<?php echo $row['lastname'] ?>' oninput='validateName()' required><br />
-                    <span class='error-message' id='lastNameError'></span>
-                    <br />
+                    <label for="lastName">Last Name:</label>
+                    <input type="test" id="lastName" name="lastName" value="<?php echo $row['lastname'] ?>" oninput="validateName()" required><br>
+                    <span class="error-message" id="lastNameError"></span><br>
                 </div>
-
                 <div>
-                    <label for='email'>Email Address:</label>
-                    <input type='email' id='email' name='email' value='<?php echo $row['email'] ?>' oninput='validateEmail()' required><br />
-                    <span class='error-message' id='emailError'></span>
-                    <br />
+                    <label for="email">Email Address:</label>
+                    <input type="email" id="email" name="email" value="<?php echo $row['email'] ?>" oninput="validateEmail()" required><br>
+                    <span class="error-message" id="emailError"></span><br>
                 </div>
-
                 <div>
-                    <label for='contact'>Contact Number:</label>
-                    <input type='tel' id='contact' name='contact' value='<?php echo $row['contact'] ?>' oninput='validateContact()' required><br />
-                    <span class='error-message' id='contactError'></span>
-                    <br />
+                    <label for="contact">Contact Number:</label>
+                    <input type="tel" id="contact" name="contact" value="<?php echo $row['contact'] ?>" oninput="validateContact()" required><br>
+                    <span class="error-message" id="contactError"></span><br>
                 </div>
-                <div class='flex-container spaced'>
-                    <input type='button' value='Cancel' onclick='window.location.href="index.php"'>
-                    <input type='submit' value='Submit'>
-                </div>
+                <input type="submit" value="Submit">
             </form>
         </div>
+        <footer>
+    webdev activity by sagarino
+</footer>
     </body>
+   
+
 </html>
